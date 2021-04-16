2596da074455d3df65469274228ef405e130de21
iepngs <iepngs@aliyun.com>
2021-04-16 17:18:10 +0800

    Update rand.go

--- rand/rand.go
+++ rand/rand.go
@@ -13,6 +13,23 @@ func RandomNumber(n int) int {
     return rand.Intn(n)
 }
 
+// 生成区间[-m, n]的安全随机数
+func RangeRand(min, max int64) int64 {
+    if min > max {
+        panic("the min is greater than max!")
+    }
+  
+    if min < 0 {
+        f64Min := math.Abs(float64(min))
+        i64Min := int64(f64Min)
+        result,_:= rand.Int(rand.Reader, big.NewInt(max + 1 + i64Min))
+  
+        return result.Int64() - i64Min
+    }
+    result,_:= rand.Int(rand.Reader, big.NewInt(max-min+1))
+    return min + result.Int64()
+}
+
 // 生成指定类型，长度为length的随机字符串
 func RandomString(randLength int, randType string) string {
     var (
